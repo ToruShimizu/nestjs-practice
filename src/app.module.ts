09fb8db0ed0bd43656f5235fcf67e307cbf7a0ed
Toru Shimizu <nobbyz.t.nn@gmail.com>
2021-02-07 20:18:35 +0900

    mongoDBの設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://trs:meta3175@cluster0.xh4zm.mongodb.net/nest?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
