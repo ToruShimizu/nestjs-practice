e317f4f8219b2a9eb2d79f2afa6da870db434a52
Toru Shimizu <nobbyz.t.nn@gmail.com>
2021-02-07 19:51:57 +0900

    pipesでバリデーションを実装

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,20 +3,15 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  // @Get('hello') 
+  // @Get('hello')
   // getHello() {
   //   const appService = new AppService()
   //   return appService.getHello()
   //   return 'hello'
-  constructor(private readonly appService: AppService) {
-
-  }
-  }
-  // constructor(private readonly appService: AppService) {}
-
+  constructor(private readonly appService: AppService) {}
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 }
-new AppController(new AppService())
+// constructor(private readonly appService: AppService) {}
