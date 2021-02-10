7a7532c5b1b2a665015b491c5ae430dcaabfcd02
Toru Shimizu <nobbyz.t.nn@gmail.com>
2021-02-10 23:45:55 +0900

    Add: jwtを追加

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+
+@Injectable()
+/** nestjs専用の関数にプロパティを渡して、返ってきたコンストラクタ関数を継承する */
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      /** jwtトークンがどこにあるか */
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      /** 有効期限が切れているトークンを有効にするか */
+      ignoreExpiration: false,
+      /** moduleにあるsecret */
+      secretOrKey: 'secret',
+    });
+  }
+  /** これは必須 */
+  /** ここに到達している時点で、ある程度チェックは済んでいるため記述は少ない */
+  async validate(payload: JwtPayload) {
+    return payload;
+  }
+}
