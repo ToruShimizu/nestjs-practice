d0818a14f5742a3febdad6551e0d84e51267eae2
Toru Shimizu <nobbyz.t.nn@gmail.com>
2021-02-10 23:46:22 +0900

    Add: UseGuardsを追加

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,9 +7,12 @@ import {
   Param,
   Delete,
   Put,
+  UseGuards,
+  Request,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
@@ -19,8 +22,10 @@ export class UsersController {
     return this.usersService.findAll();
   }
   @Get(':username')
-  findOne(@Param('username') username: string) {
-    return this.usersService.findOne(username);
+  @UseGuards(AuthGuard('jwt'))
+  findOne(@Param('username') username: string, @Request() req: any) {
+    /** トークンを持ったユーザー */
+    return req.user;
   }
   @Post()
   create(@Body(ValidationPipe) createUser: CreateUserDto) {
